31f19c45385ff851b3f392565c4ba1f8bf04192b
apotter96 <alex364981@gmail.com>
2014-09-08 22:07:30 -0400

    code cleanup

--- src/com/AandC/Power/PlanList.java
+++ src/com/AandC/Power/PlanList.java
@@ -18,41 +18,39 @@ public class PlanList extends Activity
 		try
 		{
 			assignList();
-		}
-		catch (IOException e) {
-			
+		} catch (IOException e) {
+			e.printStackTrace();
 		} catch (InvalidBibPlanException e) {
-			
+			e.printStackTrace();
 		} catch (NullPointerException e) {
-			
+			e.printStackTrace();
 		}
 	}
 	void assignList() throws IOException, InvalidBibPlanException {
 		listView = (ListView) findViewById(R.id.listPlans);
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-																android.R.layout.simple_list_item_1, android.R.id.text1, BibPlanParser.getInstalledPlans());
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, 
+			android.R.id.text1, 
+			BibPlanParser.getInstalledPlans());
 		// Assign adapter to ListView
 		listView.setAdapter(adapter); 
-
 		// ListView Item Click Listener
 		listView.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+							int position, long id) {
+				// ListView Clicked item index
+				int itemPosition = position;
 
-				@Override
-				public void onItemClick(AdapterView<?> parent, View view,
-										int position, long id) {
-					// ListView Clicked item index
-					int itemPosition     = position;
+				// ListView Clicked item value
+				String  itemValue = (String) listView.getItemAtPosition(position);
 
-					// ListView Clicked item value
-					String  itemValue    = (String) listView.getItemAtPosition(position);
+				// Show Alert 
+				Toast.makeText(getApplicationContext(),
+								  "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
+					.show();
 
-					// Show Alert 
-					Toast.makeText(getApplicationContext(),
-								   "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
-						.show();
+			}
 
-				}
-
-			}); 
+		}); 
 	}
 }
