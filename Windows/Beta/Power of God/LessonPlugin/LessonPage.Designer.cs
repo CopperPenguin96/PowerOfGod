2a8db526dd24e9081ec5e78e756e9a3456acd454
apotter96 <apotter96@powerofgodonline.net>
2016-10-10 14:50:55 -0400

    Sorry... this is Pre-Release b1a008 All good now :)
    Signed-off-by: apotter96 <apotter96@powerofgodonline.net>

--- Windows/Beta/Power of God/LessonPlugin/LessonPage.Designer.cs
+++ Windows/Beta/Power of God/LessonPlugin/LessonPage.Designer.cs
@@ -31,6 +31,9 @@ namespace LessonPlugin
         private void InitializeComponent()
         {
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroListView1 = new MetroFramework.Controls.MetroListView();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
             this.SuspendLayout();
             // 
             // webBrowser1
@@ -38,17 +41,54 @@ namespace LessonPlugin
             this.webBrowser1.Location = new System.Drawing.Point(12, 12);
             this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
             this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(592, 398);
+            this.webBrowser1.Size = new System.Drawing.Size(592, 247);
             this.webBrowser1.TabIndex = 0;
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.Location = new System.Drawing.Point(12, 266);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(275, 147);
+            this.metroLabel1.TabIndex = 1;
+            this.metroLabel1.Text = "About this message-------------------------\r\n\r\nName: {Name}\r\nDate: {Date}";
+            this.metroLabel1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroLabel1.WrapToLine = true;
+            // 
+            // metroListView1
+            // 
+            this.metroListView1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.metroListView1.FullRowSelect = true;
+            this.metroListView1.Location = new System.Drawing.Point(294, 266);
+            this.metroListView1.MultiSelect = false;
+            this.metroListView1.Name = "metroListView1";
+            this.metroListView1.OwnerDraw = true;
+            this.metroListView1.Size = new System.Drawing.Size(310, 113);
+            this.metroListView1.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.metroListView1.TabIndex = 2;
+            this.metroListView1.UseCompatibleStateImageBehavior = false;
+            this.metroListView1.UseSelectable = true;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(502, 385);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(102, 28);
+            this.metroButton1.TabIndex = 3;
+            this.metroButton1.Text = "Watch Live";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
             // 
             // LessonPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(616, 422);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.metroListView1);
+            this.Controls.Add(this.metroLabel1);
             this.Controls.Add(this.webBrowser1);
             this.Name = "LessonPage";
-            this.Text = "LessonPage";
+            this.Text = "About this message--------";
             this.ResumeLayout(false);
 
         }
@@ -56,5 +96,8 @@ namespace LessonPlugin
         #endregion
 
         private WebBrowser webBrowser1;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroListView metroListView1;
+        private MetroFramework.Controls.MetroButton metroButton1;
     }
 }
--- Windows/Beta/Power of God/LessonPlugin/LessonPage.cs
+++ Windows/Beta/Power of God/LessonPlugin/LessonPage.cs
@@ -1,14 +1,40 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
 using Power_of_God_Lib.GUI;
 
 namespace LessonPlugin
 {
     public partial class LessonPage : PowerTabPage
     {
+        private string aboutText = "";
         public LessonPage()
         {
             InitializeComponent();
+            webBrowser1.Navigated += webBrowser1_Navigated;
             webBrowser1.Navigate("https://www.youtube.com/embed/hV__1XE4tq4");
+            aboutText = metroLabel1.Text;
+            
+
+        }
+
+        private void webBrowser1_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            var DocumentTitle = webBrowser1.DocumentTitle.Substring(0, webBrowser1.DocumentTitle.LastIndexOf("-") - 2);
+            metroLabel1.Text = metroLabel1.Text.Replace("{Name}", DocumentTitle.Substring(DocumentTitle.IndexOf("\"") + 1));
+            metroLabel1.Text = metroLabel1.Text.Replace("{Date}", DocumentTitle.Substring(DocumentTitle.IndexOf("-") + 1, DocumentTitle.LastIndexOf("-") - 14));
+        }
+
+        private void metroButton1_Click(object sender, System.EventArgs e)
+        {
+            var result = MessageBox.Show("This is going to open your webbrowser. Is that ok?", "Are you sure?",
+                MessageBoxButtons.YesNo);
+            switch (result)
+            {
+                case DialogResult.Yes:
+                    System.Diagnostics.Process.Start("https://www.youtube.com/channel/UCVayQj2y7NeL7t83wzWNSAA");
+                    break;
+            }
         }
     }
 }
